e0a62ca1e5eaf720933aa36ecb46524fc778bcdd
CiroAlfa <cimonrincon01@gmail.com>
2024-01-29 13:07:48 -0500

    Post Category Page & Get All categories API part11

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,26 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DemoAngularMaterailModule } from '../DemoAngularMaterialModule';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DemoAngularMaterailModule
   ]
 })
 export class AdminModule { }
+
