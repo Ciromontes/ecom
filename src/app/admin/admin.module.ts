a53bcb40ef3f7d3b8a13868b7f16a4886c53c53f
CiroAlfa <cimonrincon01@gmail.com>
2024-01-29 18:16:18 -0500

    Post Product Page, Image Preview & Category Selection part 13

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,13 +8,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemoAngularMaterailModule } from '../DemoAngularMaterialModule';
 import { PostCategoryComponent } from './components/post-category/post-category.component';
+import { PostProductComponent } from './components/post-product/post-product.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     DashboardComponent,
-    PostCategoryComponent
+    PostCategoryComponent,
+    PostProductComponent
   ],
   imports: [
     CommonModule,
--- src/app/admin/service/admin.service.ts
+++ src/app/admin/service/admin.service.ts
@@ -5,7 +5,6 @@ import { UserStorageService } from 'src/app/services/storage/user-storage.servic
 
 const BASIC_URL = "http://localhost:8080/"
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,13 +12,24 @@ export class AdminService {
 
   constructor(private http: HttpClient) { }
 
-
-
   addCategory(categoryDto:any): Observable<any>{
     return this.http.post(BASIC_URL + 'api/admin/category', categoryDto,{
       headers: this.createAuthorizationHeader(),
     })
   }
+
+  getAllCategories(): Observable<any>{
+    return this.http.get(BASIC_URL + 'api/admin',{
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
+  addProduct(productDto:any): Observable<any>{
+    return this.http.post(BASIC_URL + 'api/admin/product', productDto,{
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
 
 
   private createAuthorizationHeader(): HttpHeaders{
