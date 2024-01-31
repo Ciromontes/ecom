a7f4151d4802818e986b22f3f1e867c246ad52d1
CiroAlfa <cimonrincon01@gmail.com>
2024-01-30 21:45:52 -0500

    Search Products By Name & Full Stack Delete Product API part 15

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent {
   products: any[]= [];
+  searchProductForm!: FormGroup;
 
-  constructor(private adminService: AdminService){}
-  ngOnInit(){
-    this.getAllProducts();
-
-  }
+  constructor(private adminService: AdminService,
+    private fb: FormBuilder){}
+    ngOnInit(){
+      this.getAllProducts();
+      this.searchProductForm = this.fb.group({
+        title: [null, [Validators.required]]
+      })
+    }
   getAllProducts(){
     this.products = [];
     this.adminService.getAllProducts().subscribe(res =>{
@@ -21,6 +27,22 @@ export class DashboardComponent {
         element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
         this.products.push(element);
       });
+      console.log(this.products)
 })
   }
+
+  submitForm(){
+    this.products = [];
+    const title = this.searchProductForm.get('title')!.value;
+    this.adminService.getAllProductsByName(title).subscribe(res =>{
+      res.forEach(element =>{
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products)
+    })
+  }
+
 }
+
+
